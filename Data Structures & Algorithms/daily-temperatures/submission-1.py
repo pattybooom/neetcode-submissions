6bbb24142012687105f0b4922a280f7cf9436530
patrick sam <139391785+pattybooom@users.noreply.github.com>
2026-06-28 18:44:19 +0100

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        temps = []
+        result = [0]*len(temperatures)
+
+        for i in range(len(temperatures)):
+  
+
+            if i == 0:
+                temps.append(temperatures[i]) # add first val to stack
+
+            else:
+
+                if temperatures[i] > temperatures[i-1]: # if current index is greater than the one before
+                    index = len(temps) -1
+                    
+
+
+                    # go backwards in the stack, remove elements until we find one larger than current
+                    while index > -1 and temperatures[i] > temps[index]:
+                        temps.pop() # remove element
+                        rIndex = i-1
+
+                        dist = 1 #distance from element we are comparing to the current temp
+                        while result[rIndex] != 0:
+                            rIndex -= 1
+                            dist += 1
+
+                        result[rIndex] = dist
+
+
+
+                        index -=1
+                        
+
+
+                    temps.append(temperatures[i])
+
+
+                else:
+                    temps.append(temperatures[i]) #otherwise we add it to the stack
+                    
+        return result
+
+
+
+            
+        
