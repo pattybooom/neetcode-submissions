43af3038dbcdbac8ca47aab167da90eacf4ad525
patrick sam <139391785+pattybooom@users.noreply.github.com>
2026-07-30 18:36:13 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxPArr = [0]
+
+
+        for i in range(1,len(prices)):
+            maxp = 0
+            for j in range(i-1,-1,-1):
+ 
+                profit = prices[i] - prices[j]
+                if profit > maxp:
+                    maxp = profit
+            maxPArr.append(maxp)
+    
+
+        return max(maxPArr)
+
+        
