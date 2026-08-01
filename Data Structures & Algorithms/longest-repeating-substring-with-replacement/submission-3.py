aa54eef207c5ae16315f6bf58557481bc75da1b0
patrick sam <139391785+pattybooom@users.noreply.github.com>
2026-08-01 18:33:45 +0100

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        #start with window size 1
+        window = 1
+
+        left = 0
+        right = 1 #left + window
+
+        letters = {}
+        letters[s[left]] = 1
+        
+        
+        #if not we slide and doagain
+
+        
+        while left + window < len(s):
+            #extend window to right checking if num of low freq characters < k
+            right = left + window
+            if s[right] not in letters:
+                letters[s[right]] = 1
+            else:
+                letters[s[right]] += 1
+
+            toChange = 0
+            for l in letters:
+                toChange += letters[l]
+            toChange -= letters[max(letters, key=lambda key: letters[key])]
+            #if valid inc window size 
+            if toChange <= k:
+                window += 1
+            else:
+                letters[s[left]] -= 1
+                left += 1
+        return window
+                
+            
+            
+
